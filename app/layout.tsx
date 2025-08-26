fdfb73df727f8eed7a40e1fe35664302bb453faa
asiusa <asiusaco@gmail.com>
2025-08-27 00:18:14 +0300

    Trigger redeploy

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import Footer from './components/Footer'
+import Header from './components/Header'
 
 export const metadata = {
   title: 'Bid Atlantic Cars',
@@ -10,6 +11,7 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="flex flex-col min-h-screen bg-white">
+        <Header />
         <main className="flex-grow">{children}</main>
         <Footer />
       </body>
